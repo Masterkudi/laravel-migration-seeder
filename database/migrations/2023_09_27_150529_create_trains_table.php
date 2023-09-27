f27ec2ffc31f681d95019ebd6e0ccc59b51884e8
masterkudi <cudini.andrea@gmail.com>
2023-09-27 20:02:16 +0200

    creazione tabella riuscita

--- database/migrations/2023_09_27_150529_create_trains_table.php
+++ database/migrations/2023_09_27_150529_create_trains_table.php
@@ -13,15 +13,17 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+
             $table->string("azienda", 100);
             $table->string("stazione_di_partenza", 100);
             $table->string("stazione_di_arrivo", 100);
-            $table->dateTime("orario_di_partenza");
-            $table->dateTime("orario_di_arrivo");
+            $table->timestamp("orario_di_partenza")->nullable(true);
+            $table->timestamp("orario_di_arrivo")->nullable(true);
             $table->string("codice_treno", 50);
-            $table->decimal("numero_carrozze", 100);
-            $table->boolean("in_orario");
-            $table->boolean("cancellato");
+            $table->decimal("numero_carrozze", 65);
+            $table->boolean("in_orario")->default(1);
+            $table->boolean("cancellato")->default(0);
+
             $table->timestamps();
         });
     }
